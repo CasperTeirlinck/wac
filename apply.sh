6b0f9ac26ccfb93678bb0c8f49ba65af7ae3cfe0
CasperTeirlinck <casperteirlinck@gmail.com>
2024-03-16 20:29:00 +0100

    ...

--- apply.sh
+++ apply.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+# Apply the dotfiles.
+# 
+# When arguments are suplied, apply using only a specific tool:
+# ./apply.sh [ansible | hm | chezmoi]
+# When using `ansible`, additionally you can give a list of tags to target:
+# ./apply.sh ansible packages,othertag
 
 if [ -z "$1" ]; then
     ansible-playbook ~/.dotfiles/main.yml -K
     home-manager switch --flake ~/.dotfiles/home-manager
-else
-    ansible-playbook main.yml -K --tags ""$1""
+    chezmoi apply
+elif [ "$1" == "ansible" ]; then
+    if [ -z "$2" ]; then
+        ansible-playbook ~/.dotfiles/main.yml -K
+    else
+        ansible-playbook ~/.dotfiles/main.yml -K --tags ""$2""
+    fi
+elif [ "$1" == "hm" ]; then
+    home-manager switch --flake ~/.dotfiles/home-manager
+elif [ "$1" == "chezmoi" ]; then
+    chezmoi apply
 fi
