ec75857fcdfcecb758b14df0b74f1fbacd1a063c
CasperTeirlinck <casperteirlinck@gmail.com>
2024-03-16 21:55:58 +0100

    ...

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -29,7 +29,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".zsh/plugins/fzf-tab".source = fetchFromGitHub {
+    ".zsh/plugins/fzf-tab".source = pkgs.fetchFromGitHub {
       owner = "Aloxaf";
       repo = "fzf-tab";
       rev = "v1.1.1";
