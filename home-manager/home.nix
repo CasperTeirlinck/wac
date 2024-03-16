daf3ea13dc6c8eb1f51590fef85ac3b330626170
CasperTeirlinck <casperteirlinck@gmail.com>
2024-03-16 23:06:20 +0100

    ...

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -46,6 +46,18 @@
     # '';
   };
 
+  services.xserver = {
+    enable = true;
+
+    windowManager.awesome = {
+      enable = true;
+      luaModules = with pkgs.luaPackages; [
+        luarocks # is the package manager for Lua modules
+        luadbi-mysql # Database abstraction layer
+      ];
+    };
+  };
+
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. If you don't want to manage your shell through Home
   # Manager then you have to manually source 'hm-session-vars.sh' located at
