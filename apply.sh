a55625dae0f06747c255a5f0469e995fa78941ee
CasperTeirlinck <casperteirlinck@gmail.com>
2024-03-16 17:25:34 +0100

    ...

--- apply.sh
+++ apply.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    ansible-playbook main.yml -K
+    ansible-playbook ~/.dotfiles/main.yml -K
+    home-manager switch --flake ~/.dotfiles/home-manager
 else
     ansible-playbook main.yml -K --tags ""$1""
 fi
