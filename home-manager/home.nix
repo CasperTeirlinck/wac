010b71b8b3a1ad9f00226be8c0c84b103287f15f
CasperTeirlinck <casperteirlinck@gmail.com>
2024-03-16 21:51:43 +0100

    ...

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -29,6 +29,13 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".zsh/plugins/fzf-tab".source = fetchFromGitHub {
+      owner = "Aloxaf";
+      repo = "fzf-tab";
+      rev = "v1.1.1";
+      hash = lib.fakeHash;
+    }
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
