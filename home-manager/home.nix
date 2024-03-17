b611de77c3c0fc5f45cd244cb9c7f1f4563a6fd1
CasperTeirlinck <casperteirlinck@gmail.com>
2024-03-17 11:43:02 +0100

    ...

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,19 +1,17 @@
 { config, pkgs, lib, nixgl, ... }:
-# let
-#   nixgl = import <nixgl> {} ;
-#    nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
-#     mkdir $out
-#     ln -s ${pkg}/* $out
-#     rm $out/bin
-#     mkdir $out/bin
-#     for bin in ${pkg}/bin/*; do
-#      wrapped_bin=$out/bin/$(basename $bin)
-#      echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
-#      chmod +x $wrapped_bin
-#     done
-#   '';
-# in
-{
+let
+   nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+    mkdir $out
+    ln -s ${pkg}/* $out
+    rm $out/bin
+    mkdir $out/bin
+    for bin in ${pkg}/bin/*; do
+     wrapped_bin=$out/bin/$(basename $bin)
+     echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
+     chmod +x $wrapped_bin
+    done
+  '';
+in {
   home.username = "casper";
   home.homeDirectory = "/home/casper";
 
@@ -22,12 +20,12 @@
   home.stateVersion = "23.11";
 
   home.packages = [
-    # nixgl.auto.nixGLDefault
+    nixgl.auto.nixGLDefault
 
     pkgs.brave
     pkgs.vscode
 
-    # (nixGLWrap pkgs.wezterm)
+    (nixGLWrap pkgs.wezterm)
     pkgs.chezmoi
     pkgs.zsh
     pkgs.fzf
