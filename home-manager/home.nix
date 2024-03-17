e362bd251362a15848f3ac01e316b28626c3ad05
CasperTeirlinck <casperteirlinck@gmail.com>
2024-03-17 11:24:38 +0100

    ...

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,17 @@
 { config, pkgs, ... }:
-
-{
+let
+  nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+    mkdir $out
+    ln -s ${pkg}/* $out
+    rm $out/bin
+    mkdir $out/bin
+    for bin in ${pkg}/bin/*; do
+     wrapped_bin=$out/bin/$(basename $bin)
+     echo "exec ${lib.getExe pkgs.nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
+     chmod +x $wrapped_bin
+    done
+  '';
+in {
   home.username = "casper";
   home.homeDirectory = "/home/casper";
 
@@ -12,7 +23,7 @@
     pkgs.brave
     pkgs.vscode
 
-    pkgs.wezterm
+    (nixGLWrap pkgs.wezterm)
     pkgs.chezmoi
     pkgs.zsh
     pkgs.fzf
