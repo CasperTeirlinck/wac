84b1f25ae91c086628d520492b6e58e3d8151b8a
CasperTeirlinck <casperteirlinck@gmail.com>
2024-03-17 12:16:56 +0100

    ...

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,18 +1,17 @@
 { config, pkgs, lib, nixgl, ... }:
-# let
-#    nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
-#     mkdir $out
-#     ln -s ${pkg}/* $out
-#     rm $out/bin
-#     mkdir $out/bin
-#     for bin in ${pkg}/bin/*; do
-#      wrapped_bin=$out/bin/$(basename $bin)
-#      echo "exec ${lib.getExe nixgl.nixGLIntel} $bin \$@" > $wrapped_bin
-#      chmod +x $wrapped_bin
-#     done
-#   '';
-# in
-{
+let
+   nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+    mkdir $out
+    ln -s ${pkg}/* $out
+    rm $out/bin
+    mkdir $out/bin
+    for bin in ${pkg}/bin/*; do
+     wrapped_bin=$out/bin/$(basename $bin)
+     echo "exec ${lib.getExe nixgl.nixGLIntel} $bin \$@" > $wrapped_bin
+     chmod +x $wrapped_bin
+    done
+  '';
+in {
   home.username = "casper";
   home.homeDirectory = "/home/casper";
 
@@ -26,7 +25,7 @@
     pkgs.brave
     pkgs.vscode
 
-    # (nixGLWrap pkgs.wezterm)
+    (nixGLWrap pkgs.wezterm)
     pkgs.chezmoi
     pkgs.zsh
     pkgs.fzf
