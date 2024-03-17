dc1d411fe8c86649c2fff76f2d817610698e83a8
CasperTeirlinck <casperteirlinck@gmail.com>
2024-03-17 12:06:32 +0100

    ...

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -27,6 +27,17 @@
         nixgl = import nixGL {
             inherit pkgs;
         };
+        nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+            mkdir $out
+            ln -s ${pkg}/* $out
+            rm $out/bin
+            mkdir $out/bin
+            for bin in ${pkg}/bin/*; do
+            wrapped_bin=$out/bin/$(basename $bin)
+            echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
+            chmod +x $wrapped_bin
+            done
+        '';
         
     in {
         homeConfigurations."casper" = home-manager.lib.homeManagerConfiguration {
