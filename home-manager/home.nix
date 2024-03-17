e8490f14e29f00492cb1f28ae811fc7bb70a5350
CasperTeirlinck <casperteirlinck@gmail.com>
2024-03-17 12:12:12 +0100

    ...

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,17 +1,17 @@
 { config, pkgs, lib, nixgl, ... }:
-# let
-#    nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
-#     mkdir $out
-#     ln -s ${pkg}/* $out
-#     rm $out/bin
-#     mkdir $out/bin
-#     for bin in ${pkg}/bin/*; do
-#      wrapped_bin=$out/bin/$(basename $bin)
-#      echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
-#      chmod +x $wrapped_bin
-#     done
-#   '';
-# in
+let
+   nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+    mkdir $out
+    ln -s ${pkg}/* $out
+    rm $out/bin
+    mkdir $out/bin
+    for bin in ${pkg}/bin/*; do
+     wrapped_bin=$out/bin/$(basename $bin)
+     echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
+     chmod +x $wrapped_bin
+    done
+  '';
+in
 {
   home.username = "casper";
   home.homeDirectory = "/home/casper";
