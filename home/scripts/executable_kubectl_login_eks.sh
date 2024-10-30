41d42bd43eddbee02f335baa3fa640d733926a59
Casper Teirlinck <casperteirlinck@gmail.com>
2024-10-30 16:40:04 +0100

    ...

---
+++ home/scripts/executable_kubectl_login_eks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cluster=""
+name=""
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    --cluster)
+        cluster="$2"
+        shift 2
+        ;;
+    --name)
+        name="$2"
+        shift 2
+        ;;
+    *)
+        echo "Invalid option: ${1:-}"
+        exit 1
+        ;;
+    esac
+done
+
+if [[ -z $cluster ]] || [[ -z $name ]]; then
+    echo "Required arguments: '--cluster' and '--name'"
+    exit 1
+fi
+
+aws eks update-kubeconfig --name $cluster
+arn=$(aws eks describe-cluster --name $cluster --query cluster.arn --output text)
+kubectl config delete-context $name || :
+kubectl config rename-context $arn $name
