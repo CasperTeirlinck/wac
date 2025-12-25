8022d22aefeac41937e5853e279e56a4e2d4aea8
Casper Teirlinck <casper.teirlinck@ond.vlaanderen.be>
2025-12-25 12:22:34 +0100

    ...

---
+++ home/code/run_onchange_code_extensions.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+plugins=(
+# --- Language support
+bbenoist.nix
+ecmel.vscode-html-css
+golang.go
+hashicorp.terraform
+ms-azuretools.vscode-docker
+ms-python.python
+ms-vscode.makefile-tools
+ms-vscode.cmake-tools
+ms-vscode.cpptools
+ms-vscode.cpptools-extension-pack
+redhat.vscode-xml
+redhat.vscode-yaml
+samuelcolvin.jinjahtml
+swiftlang.swift-vscode
+tamasfe.even-better-toml
+timonwong.shellcheck
+
+# --- Language support - Python
+ms-python.vscode-pylance
+ms-python.mypy-type-checker
+ms-python.black-formatter
+ms-python.debugpy
+ms-python.isort
+charliermarsh.ruff
+ms-toolsai.jupyter
+ms-toolsai.jupyter-keymap
+ms-toolsai.jupyter-renderers
+ms-toolsai.vscode-jupyter-cell-tags
+ms-toolsai.vscode-jupyter-slideshow
+njpwerner.autodocstring
+
+# --- Tooling
+bruno-api-client.bruno
+editorconfig.editorconfig
+grapecity.gc-excelviewer
+ms-vsliveshare.vsliveshare
+platformio.platformio-ide
+# elijah-potter.harper
+ms-vscode-remote.remote-containers
+ms-vscode-remote.remote-ssh
+ms-vscode-remote.remote-ssh-edit
+ms-vscode-remote.remote-wsl
+ms-vscode.remote-explorer
+
+# --- Git
+eamodio.gitlens
+mhutchie.git-graph
+
+# --- Theming
+aaron-bond.better-comments
+kamikillerto.vscode-colorize
+
+# --- AI
+github.copilot
+github.copilot-chat
+
+# --- Misc
+cs50.vscode-presentation-mode
+)
+
+for plugin in ${plugins[@]}; do
+  code --install-extension "$plugin" --force
+done
