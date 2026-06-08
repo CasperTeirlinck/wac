a2fd94196e7e28c1a0601c6de4ba1ad819818818
Casper Teirlinck <casper.teirlinck@ond.vlaanderen.be>
2026-06-08 14:42:41 +0200

    ...

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -10,7 +10,24 @@ map({ "n", "i", "v" }, "<C-a><Down>", "<cmd>TmuxNavigateDown<cr>", { desc = "Tmu
 map({ "n", "i", "v" }, "<C-a><Up>", "<cmd>TmuxNavigateUp<cr>", { desc = "Tmux Navigate Up" })
 map({ "n", "i", "v" }, "<C-a><Right>", "<cmd>TmuxNavigateRight<cr>", { desc = "Tmux Navigate Right" })
 
--- map({ "n", "i", "v" }, "<C-a>h", "<cmd>TmuxNavigateLeft<cr>", { desc = "Tmux Navigate Left" })
--- map({ "n", "i", "v" }, "<C-a>j", "<cmd>TmuxNavigateDown<cr>", { desc = "Tmux Navigate Down" })
--- map({ "n", "i", "v" }, "<C-a>k", "<cmd>TmuxNavigateUp<cr>", { desc = "Tmux Navigate Up" })
--- map({ "n", "i", "v" }, "<C-a>l", "<cmd>TmuxNavigateRight<cr>", { desc = "Tmux Navigate Right" })
+-- Non-vim-style insert-mode selection.
+-- Enter Visual mode + letter motion, then <C-g> toggles to Select mode
+-- so typing replaces the selection. Letter motions are used (not arrow
+-- keys) because keymodel=stopsel cancels Select mode on unshifted special keys.
+map("i", "<S-Left>",  "<C-o>vh<C-g>", { desc = "Select character left" })
+map("i", "<S-Right>", "<C-o>vl<C-g>", { desc = "Select character right" })
+map("i", "<S-Up>",    "<C-o>vk<C-g>", { desc = "Select line up" })
+map("i", "<S-Down>",  "<C-o>vj<C-g>", { desc = "Select line down" })
+map("i", "<S-Home>",  "<C-o>v0<C-g>", { desc = "Select to start of line" })
+map("i", "<S-End>",   "<C-o>v$<C-g>", { desc = "Select to end of line" })
+
+-- Word motion in insert mode. <C-Left>/<C-Right> on Linux/Windows;
+-- Ghostty translates Cmd+arrow into the same CSI sequences on macOS.
+-- <Cmd>...<CR> stays in insert mode (no InsertLeave/Enter cycle), so
+-- completion plugins don't re-trigger on the cursor move.
+map("i", "<C-Left>",    "<Cmd>normal! b<CR>", { desc = "Move word left" })
+map("i", "<C-Right>",   "<Cmd>normal! w<CR>", { desc = "Move word right" })
+-- Word selection: enter Visual, extend by word, then toggle to Select
+-- mode so typing replaces the selection.
+map("i", "<C-S-Left>",  "<C-o>vb<C-g>", { desc = "Select word left" })
+map("i", "<C-S-Right>", "<C-o>ve<C-g>", { desc = "Select word right" })
--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Non-vim-style selection: shifted special keys start Select mode where
+-- typing a printable character replaces the selection.
+vim.opt.keymodel = "startsel,stopsel"
+vim.opt.selectmode = "key,mouse"
+vim.opt.selection = "exclusive"
+-- Lets the cursor sit one past end-of-line in normal/visual mode, matching
+-- insert mode. Without this, Shift+Right at end of line selects the wrong char.
+vim.opt.virtualedit = "onemore"
